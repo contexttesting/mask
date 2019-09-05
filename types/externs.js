db5fc010e5815b33907e3293c4bd5e861e314322
Anton <anton@adc.sh>
2019-09-05 14:41:53 +0300

    fns jsdoc

--- types/externs.js
+++ types/externs.js
@@ -27,40 +27,10 @@ _contextTesting.MaskConfig.prototype.context = function() {}
  */
 _contextTesting.MaskConfig.prototype.persistentContext = function() {}
 /**
- * A possibly async function which returns results of a test. If it outputs a string, it will be compared against the `expected` property of the mask using string comparison. If it outputs an object, its deep equality with `expected` can be tested by adding `'expected'` to the `jsonProps`. Otherwise, the result must be mapped for comparison with `expected` using the `mapActual` method.
- * @type {(function(this:_contextTesting.MaskContext, ..._contextTesting.Context): *|!Promise)|undefined}
- */
-_contextTesting.MaskConfig.prototype.getResults = function(...args) {}
-/**
- * A possibly async function which returns a _Transform_ stream to be ended with the input specified in the mask's result. Its output will be accumulated and compared against the expected output of the mask.
- * @type {(function(this:_contextTesting.MaskContext, ..._contextTesting.Context): !(stream.Transform|Promise<!stream.Transform>))|undefined}
- */
-_contextTesting.MaskConfig.prototype.getTransform = function(...args) {}
-/**
- * A possibly async function which returns a _Readable_ stream constructed with the input from the mask. Its output will be stored in memory and compared against the expected output of the mask.
- * @type {(function(this:_contextTesting.MaskContext, ..._contextTesting.Context): !(stream.Readable|Promise<!stream.Readable>))|undefined}
- */
-_contextTesting.MaskConfig.prototype.getReadable = function(...args) {}
-/**
  * The path to the module to fork with the mask's input split by whitespace as arguments, output of which will be compared against the `code`, `stdout` and `stderr` properties of the mask. Arguments with whitespace should be wrapped in speech marks, i.e. `'` or `"`. Additionally, `ForkConfig` with `module`, `getArgs`, `options` and `getOptions` properties can be passed for more control of how the fork will be started.
  * @type {(string|!_contextTesting.ForkConfig)|undefined}
  */
 _contextTesting.MaskConfig.prototype.fork
-/**
- * A function which should return a configuration for [`assert-throws`](https://github.com/artdecocode/assert-throws), including `fn` and `args`, when testing an error.
- * @type {(function (this:_contextTesting.MaskContext, ..._contextTesting.Context): _assertThrows.Config)|undefined}
- */
-_contextTesting.MaskConfig.prototype.getThrowsConfig = function(...args) {}
-/**
- * The function to get a value to test against `expected` mask property from results returned by `getResults`.
- * @type {(function(*): string)|undefined}
- */
-_contextTesting.MaskConfig.prototype.mapActual = function(arg0) {}
-/**
- * A possibly async function containing any addition assertions on the results. The results from `getResults` and a map of expected values extracted from the mask's result (where `jsonProps` are parsed into JS objects) will be passed as arguments.
- * @type {(function(*, !Object<string, *>): !Promise|undefined)|undefined}
- */
-_contextTesting.MaskConfig.prototype.assertResults = function(arg0, arg1) {}
 /**
  * The properties of the mask to parse as _JSON_ values.
  * @type {(!Array<string>)|undefined}
@@ -86,3 +56,33 @@ _contextTesting.MaskConfig.prototype.propStartRe
  * @type {(!RegExp)|undefined}
  */
 _contextTesting.MaskConfig.prototype.propEndRe
+/**
+ * A possibly async function which returns results of a test. If it outputs a string, it will be compared against the `expected` property of the mask using string comparison. If it outputs an object, its deep equality with `expected` can be tested by adding `'expected'` to the `jsonProps`. Otherwise, the result must be mapped for comparison with `expected` using the `mapActual` method.
+ * @type {(function(_contextTesting.MaskContext,_contextTesting.Context): *|!Promise)|undefined}
+ */
+_contextTesting.MaskConfig.prototype.getResults = function(this, ...args) {}
+/**
+ * A possibly async function which returns a _Transform_ stream to be ended with the input specified in the mask's result. Its output will be accumulated and compared against the expected output of the mask.
+ * @type {(function(_contextTesting.MaskContext,_contextTesting.Context): !(stream.Transform|Promise<!stream.Transform>))|undefined}
+ */
+_contextTesting.MaskConfig.prototype.getTransform = function(this, ...args) {}
+/**
+ * A possibly async function which returns a _Readable_ stream constructed with the input from the mask. Its output will be stored in memory and compared against the expected output of the mask.
+ * @type {(function(_contextTesting.MaskContext,_contextTesting.Context): !(stream.Readable|Promise<!stream.Readable>))|undefined}
+ */
+_contextTesting.MaskConfig.prototype.getReadable = function(this, ...args) {}
+/**
+ * A function which should return a configuration for [`assert-throws`](https://github.com/artdecocode/assert-throws), including `fn` and `args`, when testing an error.
+ * @type {(function(_contextTesting.MaskContext,_contextTesting.Context): _assertThrows.Config)|undefined}
+ */
+_contextTesting.MaskConfig.prototype.getThrowsConfig = function(this, ...args) {}
+/**
+ * The function to get a value to test against `expected` mask property from results returned by `getResults`.
+ * @type {(function(*): string)|undefined}
+ */
+_contextTesting.MaskConfig.prototype.mapActual = function(result) {}
+/**
+ * A possibly async function containing any addition assertions on the results. The results from `getResults` and a map of expected values extracted from the mask's result (where `jsonProps` are parsed into JS objects) will be passed as arguments.
+ * @type {(function(*,!Object<string, *>): !Promise|undefined)|undefined}
+ */
+_contextTesting.MaskConfig.prototype.assertResults = function(actual, expected) {}
