48586f9f7270cc238c73aafd5798773ae1bdf7b2
Anton <anton@adc.sh>
2020-02-22 15:45:05 +0000

    t

--- test/mask/requirements.js
+++ test/mask/requirements.js
@@ -1,13 +1,13 @@
 import makeTestSuite from '@zoroaster/mask'
 import Context from '../context'
 
-export default makeTestSuite(`!test/result/requirements/default-${Context.version}`, {
+export default makeTestSuite(`test/result/requirements/default-${Context.version}`, {
   fork: {
     module: 'node_modules/zoroaster/depack/bin/zoroaster.js',
   },
 })
 
-export const getResults = makeTestSuite(`!test/result/requirements/get-results`, {
+export const getResults = makeTestSuite('test/result/requirements/get-results', {
   fork: {
     module: 'node_modules/zoroaster/depack/bin/zoroaster.js',
   },
